40a5ec0995ebcc8b0b8ea1f2f8c08272a19d478c
jgorazda <jan.gorazda@gmail.com>
2019-05-22 00:00:27 +0200

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,6 +1,9 @@
+import json
 import random
 
 import boto3
+from boto3.dynamodb.conditions import Key
+from decimal import Decimal
 
 from config.config import get_config
 from fetcher.fetcher import Fetcher
@@ -22,36 +25,49 @@ def lambda_handler(event, context):
 
     fetched_tweets = []
 
-    for screen_name in screen_names:
-        latest_user_tweet_id = None
-        results = table.query(KeyConditionExpression={'user': {"screen_name": screen_name}},
-                              ScanIndexForward=False,
-                              Limit=1)
-        if results:
-            print("DynamoDB query results: " + results)
-            latest_user_tweet_id = results[0]["id"]
-        print("Latest user tweet id: " + latest_user_tweet_id)
-
-        # TODO - change since_id
-        fetched_tweets += fetcher.get_user_timeline_tweets(screen_name=screen_name,
-                                                           since_id=None)
+    # for screen_name in screen_names:
+    #     latest_user_tweet_id = None
+    #     results = table.query(KeyConditionExpression=Key('{"user": {"screen_name")').eq(screen_name),
+    #                           ScanIndexForward=False,
+    #                           Limit=1)
+    #     if results:
+    #         print("DynamoDB query results: " + results)
+    #         latest_user_tweet_id = results[0]["id"]
+    #     print("Latest user tweet id: " + latest_user_tweet_id)
+    #
+    #     # TODO - change since_id
+    #     fetched_tweets += fetcher.get_user_timeline_tweets(screen_name=screen_name,
+    #                                                        since_id=None)
 
     for tag in tags:
-        latest_tag_tweet_id = None
-        results = table.query(KeyConditionExpression={'entities': {"hashtags": tag}},
-                              ScanIndexForward=False,
-                              Limit=1)
-        if results:
-            print("DynamoDB query results: " + results)
-            latest_tag_tweet_id = results[0]["id"]
-        print("Latest user tweet id: " + latest_tag_tweet_id)
+        # latest_tag_tweet_id = None
+        # results = table.query(KeyConditionExpression=Key('{"entities": {"hashtags")').eq(tag),
+        #                       ScanIndexForward=False,
+        #                       Limit=1)
+
+        # results = table.query(ScanIndexForward=False,
+        #                       Limit=1)
+        # if results:
+        #     print("DynamoDB query results: " + results)
+        #     latest_tag_tweet_id = results[0]["id"]
+        # print("Latest user tweet id: " + latest_tag_tweet_id)
 
         # TODO - change since_id
         fetched_tweets += fetcher.get_tweets_by_term(term=tag,
                                                      since_id=None,
                                                      since=since)
 
+    print("Writing to DynamoDb")
     # write to DynamoDB
+    first_tweet = True
     with table.batch_writer() as batch:
         for tweet in fetched_tweets:
-            batch.put_item(Item=dict(tweet._json))
+            if first_tweet:
+                print("My first tweet is:")
+                print(tweet._json)
+                first_tweet = False
+            print("Adding tweet ({}) to batch".format(tweet._json["id"]))
+            data = tweet._json
+            data = json.loads(json.dumps(data), parse_float=Decimal)
+            batch.put_item(Item=data)
+    print("Finished writing to DynamoDb")
